f16c6a71f85e5e53e983622eeb3313cfb92e84be
Bl00mstar <54003799+Bl00mstar@users.noreply.github.com>
2020-12-20 22:10:09 +0100

    Setup styling

---
+++ src/components/theme/Theme.js
@@ -0,0 +1,11 @@
+export default {
+  colors: {
+    menu: '#0D0D0D',
+    ground: '#101010',
+    active: '#ed6337',
+    icon: '#6b6e70',
+    light: '#ffffff',
+    form: '#2b2b2b',
+    error: '#961227',
+  },
+};
